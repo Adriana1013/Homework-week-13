810f35d7ecbb08d037e25718fe9d56c52b768ba6
Adriana Villegas <villegas.a102@gmail.com>
2022-09-11 23:13:57 -0500

    working through index.js and updating columns in product.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -16,6 +16,10 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
   },
   },
   {
