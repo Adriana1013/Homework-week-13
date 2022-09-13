7d8b681db3f02fbb45b6aa556df1980c9fb9d796
Adriana Villegas <villegas.a102@gmail.com>
2022-09-12 23:29:45 -0500

    creating the routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,17 +16,13 @@ onDelete: 'CASCADE'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-through: {
-  model: ProductTag,
+through: ProductTag,
   foreignKey: 'product_id',
-},
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-through: {
-  model: ProductTag,
+through: ProductTag,
   foreignKey: 'tag_id',
-}
 });
 
 module.exports = {
